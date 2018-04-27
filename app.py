5e554861456277814b7458cbfcfb12b9463af918
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-04-27 15:05:26 -0500

    init mysql

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask import current_app
 #from flask_mysqldb import MySQL
 
 import pymysql.cursors
+import os
 
 app = Flask(__name__)
 
@@ -21,16 +22,73 @@ app = Flask(__name__)
 #app.config['MYSQL_DATABASE_DB'] = 'CVSERVICE'
 #app.config['MYSQL_DATABASE_HOST'] = 'cvservice-mysql:5001'
 #mysql = MySQL()
-#mysql.init_app(app)
-
-
-
-
-
+#mysql.init_app(app
 
 
 
 from blueprints.api import api
+
+
+
+def get_mysql_connection():
+    connection = pymysql.connect(host='cv-service-mysql',
+                                 user='root',
+                                 password='my-secret-pw',
+                                 db='CVSERVICE',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor,
+                                 autocommit=True)
+    return connection
+    
+    
+    
+
+
+schema_sql = ""
+with open('/schema/schema.sql', 'r') as myfile:
+    schema_sql=myfile.readlines()
+
+schema_sql_array = [x.strip() for x in schema_sql] 
+
+example_sql=""
+if os.stat('/schema/example.sql'):
+    with open('/schema/example.sql', 'r') as myfile:
+        example_sql=myfile.readlines()
+
+    example_sql_array = [x.strip() for x in example_sql] 
+
+connection = get_mysql_connection()
+
+try:
+
+    #https://pypi.org/project/PyMySQL/
+
+    with connection.cursor() as cursor:
+        #sql = '''SELECT name FROM terms;'''
+        
+        for line in schema_sql_array:
+            if line != "":
+                print("line: " +line)
+                cursor.execute(line)
+        
+        
+    if  example_sql != "":
+          with connection.cursor() as cursor:
+              #sql = '''SELECT name FROM terms;'''
+              for line in example_sql_array:
+                  if line != "":
+                      print("line: " +line)
+                      cursor.execute(line)
+              
+              
+        
+finally:
+    connection.close()
+       
+
+
+
+
 
 
 app.register_blueprint(api, url_prefix='/api')
