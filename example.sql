f8955946576ef8e2a1d46e1343295b9b13f1347f
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-04-27 14:28:57 -0500

    added files

---
+++ example.sql
@@ -0,0 +1,13 @@
+USE CVSERVICE;
+INSERT INTO terms (name) VALUES("BIS");
+INSERT INTO terms (name) VALUES("CLS");
+INSERT INTO terms (name) VALUES("EGS");
+INSERT INTO terms (name) VALUES("HRS");
+INSERT INTO terms (name) VALUES("PSC");
+
+
+INSERT INTO synonyms (id, synonym) VALUES(1, "Business & Information Services");
+INSERT INTO synonyms (id, synonym) VALUES(2, "Computing, Environment, & Life Sciences");
+INSERT INTO synonyms (id, synonym) VALUES(3, "Energy & Global Security");
+INSERT INTO synonyms (id, synonym) VALUES(4, "Human Resource Services");
+INSERT INTO synonyms (id, synonym) VALUES(5, "Photon Sciences");
